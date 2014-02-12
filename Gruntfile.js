d81032873b859f51481c77d157698c06434c3d55
Ewelina <ewelina.kieley@gmail.com>
2014-02-11 17:03:19 -0800

    added jshint to my gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,24 @@ module.exports = function(grunt) {
       },
 
       all: { src: ['test/**/*.js'] }
+    },
+      jshint: {
+  all: ['gruntfile.js', 'test/array_indexOf_spec.js'],
+  options: {
+    globals: {
+      jQuery: true,
+      console: true,
+      module: true
     }
+  }
+}
   });
 
   // For this to work, you need to have run `npm install grunt-simple-mocha`
   grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Add a default task. This is optional, of course :)
-  grunt.registerTask('default', 'simplemocha');
+  grunt.registerTask('default', ['simplemocha','jshint']);
 
 };
